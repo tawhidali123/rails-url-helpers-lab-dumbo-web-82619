105af83267dd6ba79cfa61719bfb052c9dcfc9e9
Tawhid Ali <tawhidali126@gmail.com>
2019-09-24 18:16:36 -0400

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :students
+  post "/students/:id/activate" to: "students#activate" as: "student_activate"
 end
